76f5e3cde0a7044ab4c3799b9c8a82816b2c180b
Ivan <io@deppa.ru>
2018-05-08 00:05:41 +0300

    Three.js cube project

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  watch: true,
+  output: {
+    path: path.resolve(__dirname, 'bundle/js'),
+    filename: 'app.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  }
+};
